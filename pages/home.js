c4e561dddcae0076b2674f7c7b766d513b9e0e0b
Dextron <tueshin12@gmail.com>
2024-01-24 00:32:07 -0800

    [FIXED] moving login button on log out page

--- pages/home.js
+++ pages/home.js
@@ -55,13 +55,15 @@ const Home = () => {
     } else {
         return (
             <div className="flex flex-col items-center justify-center h-screen">
-                <div className="text-xl font-bold mb-4">You are not logged in.</div>
-                <button
-                    className="px-2 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-                    onClick={handleLoginClick}
-                >
-                    Log In
-                </button>
+                <div className='flex justify-normal flex-col fixed'>
+                    <div className="text-xl font-bold mb-4">You are not logged in.</div>
+                    <button
+                        className="px-2 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                        onClick={handleLoginClick}
+                    >
+                        Log In
+                    </button>
+                </div>
                 <div style={{ position: 'fixed', zIndex: '-1' }}>
                     <AtomicSpinner
                         atomSize={window.innerWidth <= 768 ? 620 : 800}
