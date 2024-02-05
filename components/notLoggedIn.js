7a017c535ed61cb4d0d4a55016cb7cb3e72f9c6e
Dextron <tueshin12@gmail.com>
2024-02-05 11:59:35 -0800

    added load before rendering all the components

--- components/notLoggedIn.js
+++ components/notLoggedIn.js
@@ -2,15 +2,23 @@ import React from 'react';
 import AtomicSpinner from 'atomic-spinner'
 import Head from 'next/head.js';
 import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
 
 
 const NotLoggedIn = () => {
     const router = new useRouter();
-    
+    const { data: session, status } = useSession();
+
     const handleLoginClick = () => {
         router.push("/");
     };
-    
+
+    if (status === "loading") return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <div><AtomicSpinner atomSize={300} electronSize={2.5} /></div>
+        </div>
+    );
+
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <Head>
